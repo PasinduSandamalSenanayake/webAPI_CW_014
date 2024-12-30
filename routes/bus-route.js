52941f8cbef53a5d9313c6fd266ec68a5819c0b9
Pasindu Sandamal Senanayake <138322414+PasinduSandamalSenanayake@users.noreply.github.com>
2024-12-30 12:00:07 +0530

    authorized bus

--- routes/bus-route.js
+++ routes/bus-route.js
@@ -1,15 +1,21 @@
 const express = require("express");
 const router = express.Router();
 const busController = require("../controllers/bus-controller");
+const { authenticate, authorize } = require("../middleware/auth");
 
 // Create a new bus
-router.post("/", busController.create_bus);
+router.post("/", authenticate, authorize(["admin"]), busController.create_bus);
 
 // Get all buses
 router.get("/", busController.get_all_buses);
 
 // Update a bus
-router.patch("/:id", busController.update_bus);
+router.patch(
+  "/:id",
+  authenticate,
+  authorize(["admin"]),
+  busController.update_bus
+);
 
 // Delete a bus
 router.delete("/:id", busController.delete_bus);
